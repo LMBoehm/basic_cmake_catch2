43ecc418a1fff5907bf70775e7c0ae400ce23f5e
Linus Boehm <linusboehm@gmail.com>
2024-02-23 12:07:57 -0500

    use different dirs for debug and release

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-mkdir -p build
-cd build
+BUILD_DIR=build
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
 cd ..
-./build/bin/main $@
+ln -s $BUILD_DIR/compile_commands.json compile_commands.json
+./$BUILD_DIR/bin/main $@
